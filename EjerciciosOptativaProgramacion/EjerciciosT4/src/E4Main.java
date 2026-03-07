e2c0f4654b2c48c9ee7b49626e54e32fce2af8a1
Rafita <afarfuenla@hotmail.com>
2026-03-07 01:07:52 +0100

    p

--- EjerciciosOptativaProgramacion/EjerciciosT4/src/E4Main.java
+++ EjerciciosOptativaProgramacion/EjerciciosT4/src/E4Main.java
@@ -4,5 +4,5 @@ public class E4Main {
     public static void main(String[] args) {
         E4BiPredicateLongString ejercicio4 = new E4BiPredicateLongString();
         ejercicio4.eje4();
-    }  
+    }
 }
